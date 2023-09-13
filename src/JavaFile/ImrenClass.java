965bffc9a20ac500cdb242aecd00f0df87628733
ImrenAsan <imrenasan@protonmail.com>
2023-09-13 19:52:33 +0200

    second commit - imren

--- src/JavaFile/ImrenClass.java
+++ src/JavaFile/ImrenClass.java
@@ -6,6 +6,8 @@ public class ImrenClass {
     public static void main(String[] args) {
 
         System.out.println("Welcome testing");
+
+        System.out.println("The course will start soon");
     }
 
 
